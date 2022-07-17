6c5a139f0d2f1d5057813741ddb13c4c48a9fbf4
yang <1027156343@qq.com>
2022-07-17 22:04:26 +0800

    push commit

--- src/main/java/com/ityang/git/GitTest.java
+++ src/main/java/com/ityang/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master test1");
         System.out.println("hot-fix commit2");
+        System.out.println("push test");
     }
 }
