9411aaba79e580e83c3cdc34245f9fa2922283cf
ittheshy <107518508+ittheshy@users.noreply.github.com>
2022-07-18 10:22:06 +0800

    pull test

--- src/main/java/com/ityang/git/GitTest.java
+++ src/main/java/com/ityang/git/GitTest.java
@@ -8,5 +8,7 @@ public class GitTest {
         System.out.println("master test1");
         System.out.println("hot-fix commit2");
         System.out.println("push test");
+        System.out.println("pull test");
+        
     }
 }
